0e9363fcf52f32a7160f4207028eaace258ca391
Pedro Ponte <pedroponte1989@gmail.com>
2024-08-26 15:36:36 +0000

    build: update deps

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import open_document
+import open_file
 import path_provider_foundation
 import printing
 import share_plus
@@ -13,6 +14,7 @@ import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   OpenDocumentPlugin.register(with: registry.registrar(forPlugin: "OpenDocumentPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
